6bc9f4094d1bd82e6fcf65cafc5a907e1fb84c58
khooitran <khooi.tr@gmail.com>
2026-06-03 15:05:23 +0700

    completed timelines

--- gsap-3-express/timelines/script.js
+++ gsap-3-express/timelines/script.js
@@ -1,6 +1,13 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-  gsap.timeline()
-    .from("h1", { opacity: 0, scale: 0, ease: "back" })
-    .from("#squares div", { xPercent: -110, stagger: 0.1, duration: 0.8, ease: "back" }, "<")
+  let tl = gsap.timeline()
+    .from("h1", { duration: 10, opacity: 0, scale: 0, ease: "back" })
+    .add("test")
+    .from("#squares div", { xPercent: -110, stagger: 0.1, duration: 0.8, ease: "back" })
+
+  document.getElementById("play").onclick = () => tl.play();
+  document.getElementById("pause").onclick = () => tl.pause();
+  document.getElementById("reverse").onclick = () => tl.reverse();
+  document.getElementById("restart").onclick = () => tl.restart();
+  document.getElementById("skip").onclick = () => tl.play("test");
 });
 
