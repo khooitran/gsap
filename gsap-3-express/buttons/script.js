ae72f0f27b4fb056af27010e4efd5dc4c5b43fd2
khooitran <khooi.tr@gmail.com>
2026-06-04 22:25:38 +0700

    complete hover text

--- gsap-3-express/buttons/script.js
+++ gsap-3-express/buttons/script.js
@@ -1,5 +1,17 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-  console.log("everything okay!");
+  gsap.defaults({ duration: 0.25 })
+  const items = document.querySelectorAll(".item");
+
+  items.forEach((item) => {
+    const tl = gsap.timeline({ paused: true })
+      .to(item.querySelector(".text"), { color: "white", x: 5, scale: 1.3, transformOrigin: "0 50%" })
+      .to(item.querySelector(".dot"), { backgroundColor: "#F93", scale: 1.3 }, "<");
+
+    item.addEventListener("mouseenter", () => { tl.play() });
+    item.addEventListener("mouseleave", () => { tl.reverse() });
+  });
+
+
 });
 
 
